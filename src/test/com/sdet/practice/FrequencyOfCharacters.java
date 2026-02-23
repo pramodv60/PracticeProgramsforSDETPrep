18c20c87476c10170ef8c2279f92731623af0b99
Pramod <pramodv60@gmail.com>
2026-02-23 19:23:22 +1100

    commmitting remaining changes

--- src/test/com/sdet/practice/FrequencyOfCharacters.java
+++ src/test/com/sdet/practice/FrequencyOfCharacters.java
@@ -1,37 +1,38 @@
 package com.sdet.practice;
 
-import java.util.HashMap;	
+import java.util.HashMap;
 
 public class FrequencyOfCharacters {
 
 	public static void main(String[] args) {
-		
+
 		String input = "DISCOMBOMBULATED";
-		
+
 		HashMap<Character, Integer> result = countFrequency(input);
-		
-		for(char eachCharacter: result.keySet()) {
-			System.out.println("Character: " + eachCharacter + " is repeated: " + result.get(eachCharacter) + " times.");
+
+		for (char eachCharacter : result.keySet()) {
+			System.out
+					.println("Character: " + eachCharacter + " is repeated: " + result.get(eachCharacter) + " times.");
 		}
 
 	}
-	
-	public static HashMap<Character, Integer> countFrequency(String input){
-		
+
+	public static HashMap<Character, Integer> countFrequency(String input) {
+
 		char[] characterArray = input.toCharArray();
 		HashMap<Character, Integer> resultMap = new HashMap<Character, Integer>();
 		int counter = 0;
-		
-		if(input.length() > 0) {
-			
-			for(char eachCharacter: characterArray) {
-				
-				if(! (resultMap.containsKey(eachCharacter)) ) {
+
+		if (input.length() > 0) {
+
+			for (char eachCharacter : characterArray) {
+
+				if (!(resultMap.containsKey(eachCharacter))) {
 					++counter;
 					resultMap.put(eachCharacter, counter);
 					counter = 0;
 				}
-				
+
 				else {
 					counter = (int) resultMap.get(eachCharacter);
 					++counter;
@@ -40,9 +41,9 @@ public class FrequencyOfCharacters {
 				}
 			}
 		}
-		
+
 		return resultMap;
-		
+
 	}
 
 }
